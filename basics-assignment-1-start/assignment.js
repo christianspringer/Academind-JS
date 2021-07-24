04446ec010fff05c63b157bb54ab4c8ec5eebd8c
Christian Springer <cfs2228@gmail.com>
2021-07-24 09:13:10 -0400

    finish assignment 1

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,20 @@
+//Assignment 
+
+//Step 1
+let userInput = 9;
+let result;
+
+//Step 2
+result = 18 + userInput;
+
+//step 3
+result = result - 5;
+result = result * 10
+result = result / 2;
+
+//step 4
+//No the userInput has not changed, we changed the result variable only in this case
+
+//step 5
+alert(userInput);
+alert(result);
