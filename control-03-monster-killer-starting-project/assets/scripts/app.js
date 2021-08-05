602dbd6eabc3c78b640f39f86343277808651306
Christian Springer <cfs2228@gmail.com>
2021-08-05 09:23:13 -0400

    implement bonus life and reset game

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -6,11 +6,28 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert('You should be dead! The bonus life saved you!');
+  }
 
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You Slayed the Monster!');
@@ -18,6 +35,10 @@ function endRound() {
     alert('The monster destroyed you!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('Both you and the monster are dead.');
+  }
+
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset();
   }
 }
 
