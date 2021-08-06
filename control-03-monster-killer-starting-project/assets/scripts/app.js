2d3c3b4c823b6fd23f5d0470d5846e244d2ccc5a
Christian Springer <cfs2228@gmail.com>
2021-08-06 12:02:12 -0400

    refactor add switch

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -35,22 +35,39 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     finalMosterHealth: monsterHealth,
     finalPlayerHealth: playerHealth,
   };
-  if (ev === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry.target = 'PLAYER';
-  } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-    logEntry.target = 'PLAYER';
-  } else if (ev === LOG_EVENT_GAME_OVER) {
-    let logEntry = {
-      event: ev,
-      value: val,
-      finalMosterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth,
-    };
+  switch(ev){
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_GAME_OVER:
+      logEntry.target = 'NONE'
+
   }
+  // if (ev === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry.target = 'PLAYER';
+  // } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry.target = 'PLAYER';
+  // } else if (ev === LOG_EVENT_GAME_OVER) {
+  //    logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     finalMosterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth,
+  //   };
+  // }
   battleLog.push(logEntry);
 }
 
