6da4552e7eccf282c9f6d1d662566ade87718457
Christian Springer <cfs2228@gmail.com>
2021-08-10 16:45:41 -0400

    working functions

--- DOING/functions-01-starting-code/app.js
+++ DOING/functions-01-starting-code/app.js
@@ -1,1 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const startGame = function () {
+  console.log('game is starting...');
+};
+
+// const person = {
+//   name: 'Christian',
+//   greet: function greet(){
+//     console.log('hello');
+//   }
+// }
+
+// person.greet();
+
+// console.dir(startGame);
+
+startGameBtn.addEventListener('click', startGame);
