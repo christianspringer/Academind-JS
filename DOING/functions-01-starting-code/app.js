f543fffaca263156fc2c2e977c6ecc456b845b05
Christian Springer <cfs2228@gmail.com>
2021-08-11 08:53:24 -0400

    function practice

--- DOING/functions-01-starting-code/app.js
+++ DOING/functions-01-starting-code/app.js
@@ -10,19 +10,19 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function () {
+const getPlayerChoice = () => {
   const selection = prompt(
     `${ROCK}, ${PAPER} OR ${SCISSORS} `,
     ''
   ).toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-    alert(`Invalid choice, we chose ${ROCK} for you.`);
-    return DEFAULT_USER_CHOICE;
+    alert(`Invalid choice, we chose ${DEFAULT_USER_CHOICE} for you.`);
+    return;
   }
   return selection;
 };
 
-const getComputerChoice = function () {
+const getComputerChoice = () => {
   const randomValue = Math.random();
   if (randomValue < 0.34) {
     return ROCK;
@@ -33,29 +33,78 @@ const getComputerChoice = function () {
   }
 };
 
-const getWinner = function (cChoice, pChoice) {
-  if (cChoice === pChoice) {
-    return RESULT_DRAW;
-  } else if (
-    (cChoice === ROCK && pChoice === PAPER) ||
-    (cChoice === PAPER && pChoice === SCISSORS) ||
-    (cChoice === SCISSORS && pChoice === ROCK)
-  ) {
-    return RESULT_PLAYER_WINS;
-  } else {
-    return RESULT_COMPUTER_WINS;
-  }
-};
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
 
-startGameBtn.addEventListener('click', function startGame() {
+// if (cChoice === pChoice) {
+//   return RESULT_DRAW;
+// } else if (
+//   (cChoice === ROCK && pChoice === PAPER) ||
+//   (cChoice === PAPER && pChoice === SCISSORS) ||
+//   (cChoice === SCISSORS && pChoice === ROCK)
+// ) {
+//   return RESULT_PLAYER_WINS;
+// } else {
+//   return RESULT_COMPUTER_WINS;
+// }
+
+startGameBtn.addEventListener('click', () => {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
-  console.log('game is starting...');
-  const playerChoice = getPlayerChoice();
-  console.log(playerChoice);
-  const computerChoice = getComputerChoice();
-  const winner = getWinner(computerChoice, playerChoice)
-  console.log(winner);
+  const pChoice = getPlayerChoice(); //might be undefined
+  const cChoice = getComputerChoice();
+  let winner;
+  if (pChoice) {
+    winner = getWinner(cChoice, pChoice);
+  } else {
+    winner = getWinner(cChoice);
+  }
+  let message = `You picked ${
+    pChoice || DEFAULT_USER_CHOICE
+  } and the computer picked ${cChoice} resulting in a `;
+  if (winner === RESULT_DRAW) {
+    message += 'draw :/';
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message += 'win! :)';
+  } else {
+    message += 'loss :(';
+  }
+  alert(message);
+  gameIsRunning = false;
 });
+
+//not game related rest operator example
+const sumUp = (resultHandler, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const number of numbers) {
+    sum += validateNumber(number);
+  }
+  resultHandler(sum);
+};
+
+const subtractUp = function() {
+  let difference;
+  for (const number of arguments) {
+    difference += number;
+  }
+  return difference;
+};
+
+const showResult = (result) => {
+  alert(`The result after adding all numbers is ${result}`);
+};
+
+sumUp(showResult, 5, 4, 11, 69, 1);
+sumUp(showResult, 12,24, 64);
